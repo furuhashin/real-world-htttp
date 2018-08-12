4a11b04d5d59994be7ac9e398eae227c632fc984
nob <yasai.saita@gmail.com>
2018-08-12 15:38:29 +0900

    [6.5 リモートプロシージャコール（RPC）]

--- server/main.go
+++ server/main.go
@@ -1,76 +1,41 @@
 package main
 
 import (
-	"fmt"
-	"io"
 	"log"
+	"net"
 	"net/http"
-	"net/http/httputil"
-	"time"
+	"net/rpc"
+	"net/rpc/jsonrpc"
 )
 
-func handlerUpgrade(w http.ResponseWriter, r *http.Request) {
-	if r.Header.Get("Connection") != "Upgrade" || r.Header.Get("Upgrade") != "MyProtocol" {
-		w.WriteHeader(400)
-		return
-	}
-	fmt.Println("Upgrade to MyProtocol")
+type Calculator int
 
-	hijacker := w.(http.Hijacker)
-	conn, readWriter, err := hijacker.Hijack()
-	if err != nil {
-		panic(err)
-		return
-	}
-	defer conn.Close()
-
-	response := http.Response{
-		StatusCode: 101,
-		Header:     make(http.Header),
-	}
-	response.Header.Set("Upgrade", "Myprotocol")
-	response.Header.Set("Connection", "Upgrade")
-	response.Write(conn)
-
-	for i := 1; i <= 10; i++ {
-		fmt.Fprintf(readWriter, "%d\n", i)
-		fmt.Println("->", i)
-		readWriter.Flush()
-		recv, err := readWriter.ReadBytes('\n')
-		if err == io.EOF {
-			break
-		}
-		fmt.Printf("<- %s", string(recv))
-		time.Sleep(500 * time.Millisecond)
-	}
+func (c *Calculator) Multiply(args Args, result *int) error {
+	log.Printf("Multiply called: %d, %d\n", args.A, args.B)
+	*result = args.A * args.B
+	return nil
 }
 
-func handleChunkedRespose(w http.ResponseWriter, r *http.Request) {
-	flusher, ok := w.(http.Flusher)
-	if !ok {
-		panic("expected http.ResponseWriter to be an http.Flusher")
-	}
-	for i := 1; i <= 10; i++ {
-		fmt.Fprintf(w, "Chunk #%d\n", i)
-		flusher.Flush()
-		time.Sleep(500 * time.Millisecond)
-	}
-	flusher.Flush()
-}
-
-func handler(w http.ResponseWriter, r *http.Request) {
-	dump, err := httputil.DumpRequest(r, true)
-	if err != nil {
-		http.Error(w, fmt.Sprint(err), http.StatusInternalServerError)
-		return
-	}
-	fmt.Println(string(dump))
-	fmt.Fprintf(w, "<html><body>hello</body></html>")
+type Args struct {
+	A, B int
 }
 
 func main() {
-	http.HandleFunc("/chunked", handleChunkedRespose)
+	calculator := new(Calculator)
+	server := rpc.NewServer()
+	server.Register(calculator)
+	http.Handle(rpc.DefaultRPCPath, server)
 	log.Println("start http listening :18888")
-	err := http.ListenAndServe(":18888", nil)
-	log.Println(err)
+	listener, err := net.Listen("tcp", ":18888")
+	if err != nil {
+		panic(err)
+	}
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			panic(err)
+		}
+		go server.ServeCodec(jsonrpc.NewServerCodec(conn))
+
+	}
 }
--- simpleget/main.go
+++ simpleget/main.go
@@ -1,102 +1,24 @@
 package main
 
 import (
-	"bufio"
-	"io"
 	"log"
-	"net"
-	"net/http"
-	"strconv"
-	"time"
+	"net/rpc/jsonrpc"
 )
 
+type Args struct {
+	A, B int
+}
+
 func main() {
-	dialer := &net.Dialer{
-		Timeout:   30 * time.Second,
-		KeepAlive: 30 * time.Second,
-	}
-	conn, err := dialer.Dial("tcp", "localhost:18888")
+	client, err := jsonrpc.Dial("tcp", "localhost:18888")
 	if err != nil {
 		panic(err)
 	}
-	defer conn.Close()
-	reader := bufio.NewReader(conn)
-
-	request, _ := http.NewRequest("GET", "http://localhost:18888/chunked", nil)
-	err = request.Write(conn)
+	var result int
+	args := &Args{4, 5}
+	err = client.Call("Calculator.Multiply", args, &result)
 	if err != nil {
 		panic(err)
 	}
-	resp, err := http.ReadResponse(reader, request)
-	if err != nil {
-		panic(err)
-	}
-	if resp.TransferEncoding[0] != "chunked" {
-		panic("wrong transfer encoding")
-	}
-
-	for {
-		sizeStr, err := reader.ReadBytes('\n')
-		if err == io.EOF {
-			break
-		}
-
-		size, err := strconv.ParseInt(string(sizeStr[:len(sizeStr)-2]), 16, 64)
-		if size == 0 {
-			break
-		}
-		if err != nil {
-			panic(err)
-		}
-		line := make([]byte, int(size))
-		reader.Read(line)
-		reader.Discard(2)
-		log.Println(" ", string(line))
-	}
-
-	// log.Println("Status:", resp.Status)
-	// log.Println("Headers:", resp.Header)
-
-	// counter := 10
-	// for {
-	// 	data, err := reader.ReadBytes('\n')
-	// 	if err == io.EOF {
-	// 		break
-	// 	}
-	// 	fmt.Println("<-", string(bytes.TrimSpace(data)))
-	// 	fmt.Fprintf(conn, "%d\n", counter)
-	// 	fmt.Println("->", counter)
-	// 	counter--
-	// }
-
-	// cert, err := ioutil.ReadFile("../zoo/ca.crt")
-	// if err != nil {
-	// 	panic(err)
-	// }
-	// certPool := x509.NewCertPool()
-	// certPool.AppendCertsFromPEM(cert)
-	// tlsConfig := &tls.Config{
-	// 	RootCAs: certPool,
-	// }
-	// tlsConfig.BuildNameToCertificate()
-
-	// client := &http.Client{
-	// 	Transport: &http.Transport{
-	// 		TLSClientConfig: tlsConfig,
-	// 	},
-	// }
-
-	// resp, err := http.Get("http://localhost:18888/chunked")
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-	// defer resp.Body.Close()
-	// reader := bufio.NewReader(resp.Body)
-	// for {
-	// 	line, err := reader.ReadBytes('\n')
-	// 	if err == io.EOF {
-	// 		break
-	// 	}
-	// 	log.Println(string(bytes.TrimSpace(line)))
-	// }
+	log.Printf("4 * 5 = %d\n", result)
 }
