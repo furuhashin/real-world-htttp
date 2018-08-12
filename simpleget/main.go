f98ea6912f3d054ab788d5581e83c9a3d8c85314
nob <yasai.saita@gmail.com>
2018-08-12 14:55:08 +0900

    [6.4.2　クライアントでの逐次受信（簡易版）]

--- simpleget/main.go
+++ simpleget/main.go
@@ -3,51 +3,48 @@ package main
 import (
 	"bufio"
 	"bytes"
-	"fmt"
 	"io"
 	"log"
-	"net"
 	"net/http"
-	"time"
 )
 
 func main() {
-	dialer := &net.Dialer{
-		Timeout:   30 * time.Second,
-		KeepAlive: 30 * time.Second,
-	}
-	conn, err := dialer.Dial("tcp", "localhost:18888")
-	if err != nil {
-		panic(err)
-	}
-	defer conn.Close()
-	reader := bufio.NewReader(conn)
+	// dialer := &net.Dialer{
+	// 	Timeout:   30 * time.Second,
+	// 	KeepAlive: 30 * time.Second,
+	// }
+	// conn, err := dialer.Dial("tcp", "localhost:18888")
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// defer conn.Close()
+	// reader := bufio.NewReader(conn)
 
-	request, _ := http.NewRequest("GET", "http://localhost:18888/upgrade", nil)
-	request.Header.Set("Connection", "Upgrade")
-	request.Header.Set("Upgrade", "MyProtocol")
-	err = request.Write(conn)
-	if err != nil {
-		panic(err)
-	}
-	resp, err := http.ReadResponse(reader, request)
-	if err != nil {
-		panic(err)
-	}
-	log.Println("Status:", resp.Status)
-	log.Println("Headers:", resp.Header)
+	// request, _ := http.NewRequest("GET", "http://localhost:18888/upgrade", nil)
+	// request.Header.Set("Connection", "Upgrade")
+	// request.Header.Set("Upgrade", "MyProtocol")
+	// err = request.Write(conn)
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// resp, err := http.ReadResponse(reader, request)
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// log.Println("Status:", resp.Status)
+	// log.Println("Headers:", resp.Header)
 
-	counter := 10
-	for {
-		data, err := reader.ReadBytes('\n')
-		if err == io.EOF {
-			break
-		}
-		fmt.Println("<-", string(bytes.TrimSpace(data)))
-		fmt.Fprintf(conn, "%d\n", counter)
-		fmt.Println("->", counter)
-		counter--
-	}
+	// counter := 10
+	// for {
+	// 	data, err := reader.ReadBytes('\n')
+	// 	if err == io.EOF {
+	// 		break
+	// 	}
+	// 	fmt.Println("<-", string(bytes.TrimSpace(data)))
+	// 	fmt.Fprintf(conn, "%d\n", counter)
+	// 	fmt.Println("->", counter)
+	// 	counter--
+	// }
 
 	// cert, err := ioutil.ReadFile("../zoo/ca.crt")
 	// if err != nil {
@@ -66,14 +63,17 @@ func main() {
 	// 	},
 	// }
 
-	// resp, err := client.Get("https://localhost:18443")
-	// if err != nil {
-	// 	panic(err)
-	// }
-	// defer resp.Body.Close()
-	// dump, err := httputil.DumpResponse(resp, true)
-	// if err != nil {
-	// 	panic(err)
-	// }
-	// log.Println(string(dump))
+	resp, err := http.Get("http://localhost:18888/chunked")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+	reader := bufio.NewReader(resp.Body)
+	for {
+		line, err := reader.ReadBytes('\n')
+		if err == io.EOF {
+			break
+		}
+		log.Println(string(bytes.TrimSpace(line)))
+	}
 }
