f62394aea932688a4e549f076d077fa1b8f17026
nob <yasai.saita@gmail.com>
2018-08-12 15:17:31 +0900

    [6.4 クライアントでの逐次処理（完全版）]

--- simpleget/main.go
+++ simpleget/main.go
@@ -2,35 +2,58 @@ package main
 
 import (
 	"bufio"
-	"bytes"
 	"io"
 	"log"
+	"net"
 	"net/http"
+	"strconv"
+	"time"
 )
 
 func main() {
-	// dialer := &net.Dialer{
-	// 	Timeout:   30 * time.Second,
-	// 	KeepAlive: 30 * time.Second,
-	// }
-	// conn, err := dialer.Dial("tcp", "localhost:18888")
-	// if err != nil {
-	// 	panic(err)
-	// }
-	// defer conn.Close()
-	// reader := bufio.NewReader(conn)
+	dialer := &net.Dialer{
+		Timeout:   30 * time.Second,
+		KeepAlive: 30 * time.Second,
+	}
+	conn, err := dialer.Dial("tcp", "localhost:18888")
+	if err != nil {
+		panic(err)
+	}
+	defer conn.Close()
+	reader := bufio.NewReader(conn)
 
-	// request, _ := http.NewRequest("GET", "http://localhost:18888/upgrade", nil)
-	// request.Header.Set("Connection", "Upgrade")
-	// request.Header.Set("Upgrade", "MyProtocol")
-	// err = request.Write(conn)
-	// if err != nil {
-	// 	panic(err)
-	// }
-	// resp, err := http.ReadResponse(reader, request)
-	// if err != nil {
-	// 	panic(err)
-	// }
+	request, _ := http.NewRequest("GET", "http://localhost:18888/chunked", nil)
+	err = request.Write(conn)
+	if err != nil {
+		panic(err)
+	}
+	resp, err := http.ReadResponse(reader, request)
+	if err != nil {
+		panic(err)
+	}
+	if resp.TransferEncoding[0] != "chunked" {
+		panic("wrong transfer encoding")
+	}
+
+	for {
+		sizeStr, err := reader.ReadBytes('\n')
+		if err == io.EOF {
+			break
+		}
+
+		size, err := strconv.ParseInt(string(sizeStr[:len(sizeStr)-2]), 16, 64)
+		if size == 0 {
+			break
+		}
+		if err != nil {
+			panic(err)
+		}
+		line := make([]byte, int(size))
+		reader.Read(line)
+		reader.Discard(2)
+		log.Println(" ", string(line))
+	}
+
 	// log.Println("Status:", resp.Status)
 	// log.Println("Headers:", resp.Header)
 
@@ -63,17 +86,17 @@ func main() {
 	// 	},
 	// }
 
-	resp, err := http.Get("http://localhost:18888/chunked")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer resp.Body.Close()
-	reader := bufio.NewReader(resp.Body)
-	for {
-		line, err := reader.ReadBytes('\n')
-		if err == io.EOF {
-			break
-		}
-		log.Println(string(bytes.TrimSpace(line)))
-	}
+	// resp, err := http.Get("http://localhost:18888/chunked")
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// defer resp.Body.Close()
+	// reader := bufio.NewReader(resp.Body)
+	// for {
+	// 	line, err := reader.ReadBytes('\n')
+	// 	if err == io.EOF {
+	// 		break
+	// 	}
+	// 	log.Println(string(bytes.TrimSpace(line)))
+	// }
 }
