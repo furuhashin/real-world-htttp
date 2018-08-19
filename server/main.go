b10f3bdfa9977600333cad041133f75463aa1866
nob <yasai.saita@gmail.com>
2018-08-19 18:45:22 +0900

    [9.3.1 サーバの実装]

--- server/main.go
+++ server/main.go
@@ -3,37 +3,67 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"math/big"
 	"net/http"
+	"time"
 )
 
-var image []byte
-
-func init() {
-	var err error
-	image, err = ioutil.ReadFile("./image.png")
-	if err != nil {
-		panic(err)
-	}
-}
+var html []byte
 
 func handlerHtml(w http.ResponseWriter, r *http.Request) {
-	pusher, ok := w.(http.Pusher)
-	if ok {
-		pusher.Push("/image", nil)
-	}
 	w.Header().Add("Content-Type", "text/html")
+<<<<<<< Updated upstream
 	fmt.Fprintf(w, `<html><body><img src="/image"></body></html>`)
+=======
+	w.Write(html)
+>>>>>>> Stashed changes
 }
 
-func handlerImage(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "image/png")
-	w.Write(image)
+func handlerPrimeSSE(w http.ResponseWriter, r *http.Request) {
+	flusher, ok := w.(http.Flusher)
+	if !ok {
+		http.Error(w, "Streaming unsupported!", http.StatusInternalServerError)
+		return
+	}
+	closeNotify := w.(http.CloseNotifier).CloseNotify()
+	w.Header().Set("Content-Type", "text/event-stream")
+	w.Header().Set("Cache-Control", "no-cache")
+	w.Header().Set("Connection", "keep-alive")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+
+	var num int64 = 1
+	for id := 1; id <= 100; id++ {
+		select {
+		case <-closeNotify:
+			fmt.Println("Connection closed from client")
+			return
+		default:
+			// do nothing
+		}
+		for {
+			num++
+			if big.NewInt(num).ProbablyPrime(20) {
+				fmt.Println(num)
+				fmt.Fprintf(w, "data: {\"id\":%d, \"number\": %d}\n\n", id, num)
+				flusher.Flush()
+				time.Sleep(time.Second)
+				break
+			}
+		}
+		time.Sleep(time.Second)
+	}
+	fmt.Println("Connection closed from server")
 }
 
 func main() {
+	var err error
+	html, err = ioutil.ReadFile("index.html")
+	if err != nil {
+		panic(err)
+	}
 	http.HandleFunc("/", handlerHtml)
-	http.HandleFunc("/image", handlerImage)
-	fmt.Println("start http listening :18443")
-	err := http.ListenAndServeTLS(":18443", "./ca/server.crt", "./ca/server.key", nil)
+	http.HandleFunc("/prime", handlerPrimeSSE)
+	fmt.Println("start http listening :18888")
+	err = http.ListenAndServe(":18888", nil)
 	fmt.Println(err)
 }
